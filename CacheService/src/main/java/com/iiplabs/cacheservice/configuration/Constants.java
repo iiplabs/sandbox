2dec5d9864cb6faa78746dd3aa176637661e3a34
Igor Ilyutkin <igor@iiplabs.com>
2016-06-02 14:01:43 -0400

    Updated default cache strategy

--- CacheService/src/main/java/com/iiplabs/cacheservice/configuration/Constants.java
+++ CacheService/src/main/java/com/iiplabs/cacheservice/configuration/Constants.java
@@ -10,7 +10,7 @@ public final class Constants {
 	
 	public static final int NEW_KEY_LEN = 25;
 	
-	public static final CacheStrategy DEFAULT_STRATEGY = CacheStrategy.FILE;
+	public static final CacheStrategy DEFAULT_STRATEGY = CacheStrategy.MEMORY;
 	
 	public static final int MEMORY_MAX = 3;
 	public static final int FILE_MAX = 3;
