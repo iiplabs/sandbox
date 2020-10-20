a1606b500335825a915cba5a59b23019d3c70db4
Igor Ilyutkin <igor@iiplabs.com>
2020-10-19 21:41:02 -0400

    sort algos

---
+++ LinkedList/src/main/java/com/iiplabs/sort/BubbleSort.java
@@ -0,0 +1,54 @@
+package com.iiplabs.sort;
+
+import java.util.stream.IntStream;
+
+public class BubbleSort {
+	
+    static void bubbleSort(int[] arr) {  
+        int n = arr.length;  
+        int temp = 0;  
+         for(int i=0; i < n; i++){  
+                 for(int j=1; j < (n-i); j++){  
+                          if(arr[j-1] > arr[j]){  
+                                 //swap elements  
+                                 temp = arr[j-1];  
+                                 arr[j-1] = arr[j];  
+                                 arr[j] = temp;  
+                         }  
+                          
+                 }  
+         }  
+  
+    }
+    
+    static void bubbleSortJava8(Integer[] arr) {
+        int n = arr.length;
+        IntStream.range(0, n - 1)
+          .flatMap(i -> IntStream.range(1, n - i))
+          .forEach(j -> {
+              if (arr[j - 1] > arr[j]) {
+                  int temp = arr[j];
+                  arr[j] = arr[j - 1];
+                  arr[j - 1] = temp;
+              }
+          });
+    }
+    
+    public static void main(String[] args) {  
+                int arr[] ={3,60,35,2,45,320,5};  
+                 
+                System.out.println("Array Before Bubble Sort");  
+                for(int i=0; i < arr.length; i++){  
+                        System.out.print(arr[i] + " ");  
+                }  
+                System.out.println();  
+                  
+                bubbleSort(arr);//sorting array elements using bubble sort  
+                 
+                System.out.println("Array After Bubble Sort");  
+                for(int i=0; i < arr.length; i++){  
+                        System.out.print(arr[i] + " ");  
+                }  
+   
+   }  
+}
