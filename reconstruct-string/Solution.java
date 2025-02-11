1a160cdec291ca02a8a8fc5c2134e5477d7ba0aa
Igor Ilyutkin <igor@iiplabs.com>
2025-02-11 12:28:30 -0500

    updated reconstruct to use graph walking

--- reconstruct-string/Solution.java
+++ reconstruct-string/Solution.java
@@ -1,61 +1,54 @@
-package com.rogers.media.supplychain.showdeliveryandtrackingapp.util;
-
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Solution {
-    public int solution(String S, String [] grid) {
-        int RECONST_NOT_POSSIBLE = -1;
 
-        int totalMoves = 0;
-
-        if (S.length() > 0) {
-            String firstChar = S.substring(0, 1);
-            int[][] firstCharAllPos = allPos(firstChar, grid);
-            if (firstCharAllPos.length == 0 || firstCharAllPos.length > 2) {
-                return  RECONST_NOT_POSSIBLE;
-            }
-
-            int[] prevPos = firstCharAllPos[0];
-            // System.out.println(String.format("firstChar - %s, firstCharAllPos.length = %d", firstChar, firstCharAllPos.length));
-            for (int i = 1; i < S.length(); i++) {
-                String nextChar = S.substring(i, i + 1);
-                int[][] nextPos = allPos(nextChar, grid);
-                // character not found
-                if (nextPos.length == 0 || nextPos.length > 2) {
-                    return RECONST_NOT_POSSIBLE;
-                }
-
-                // System.out.println(String.format("nextChar - %s, nextPos.length = %d", nextChar, nextPos.length));
-                /* for (int ii = 0; ii < nextPos.length; ii++) {
-                    System.out.println(String.format("%s - %s", nextChar, Arrays.toString(nextPos[ii])));
-                } */
-
-                int[] minDistPos = findMinDistPos(prevPos, nextPos);
-                int dist = calcDist(prevPos, minDistPos);
-                System.out.println(dist);
-
-                totalMoves = totalMoves + dist;
-
-                prevPos = minDistPos;
-            }
+    class Position {
+        int x;
+        int y;
+        Position(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+        int[] toArray() {
+            return new int[] { x, y };
         }
 
-        return totalMoves > 0 ? totalMoves : RECONST_NOT_POSSIBLE;
+        @Override
+        public String toString() {
+            return String.format("x=%d,y=%d", x, y);
+        }
     }
 
-    int[] findMinDistPos(int[] prevPos, int[][] allPos) {
-        int minDistIndex = 0;
-        int minDist = calcDist(prevPos, allPos[minDistIndex]);
-
-        for (int ii = 0; ii < allPos.length; ii++) {
-            int dist = calcDist(prevPos, allPos[ii]);
-            if (dist < minDist) {
-                minDist = dist;
-                minDistIndex = ii;
+    Map<String, Collection<Position>> buidDict(String [] grid) {
+        Map<String, Collection<Position>> dict = new HashMap<>();
+        for (int yy = 0; yy < grid.length; yy++) {
+            String gridElem = grid[yy];
+            for (int xx = 0; xx < gridElem.length(); xx++) {
+                String gridChar = gridElem.substring(xx, xx + 1);
+                if (!gridChar.equalsIgnoreCase(".")) {
+                    Collection<Position> charAllPos = new ArrayList<>();
+                    if (dict.containsKey(gridChar)) {
+                        charAllPos.addAll(dict.get(gridChar));
+                    }
+                    charAllPos.add(new Position(xx, yy));
+                    dict.put(gridChar, charAllPos);
+                }
             }
         }
+        return dict;
+    }
 
-        return allPos[minDistIndex];
+    boolean checkSourceString(String S, Map<String, Collection<Position>> dict) {
+        for (int ss = 0; ss < S.length(); ss++) {
+            String ch = S.substring(ss, ss + 1);
+            if (!dict.containsKey(ch)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     int calcDist(int[] prevPos, int[] pos) {
@@ -64,33 +57,59 @@ public class Solution {
         return xxDist + yyDist;
     }
 
-    int[][] allPos(String ch, String[] grid) {
-        int[][] allPos = new int[][] {};
-
-        for (int yy = 0; yy < grid.length; yy++) {
-            String gridElem = grid[yy];
-            for (int xx = 0; xx < gridElem.length(); xx++) {
-                String gridChar = gridElem.substring(xx, xx + 1);
-                if (gridChar.equals(ch)) {
-                    allPos = addNewPos(allPos, new int[] {xx, yy});
-                }
-            }
+    int search(int[] curPos, int index, String S, Map<String, Collection<Position>> dict) {
+        if (index == S.length() - 1) {
+            return 0;
         }
+        int ret = Integer.MAX_VALUE;
 
-        return allPos;
+        String curChar = S.substring(index, index + 1);
+        String nextChar = S.substring(index + 1, index + 1 + 1);
+        if (curChar.equals(nextChar)) {
+            return search(curPos, index + 1, S, dict);
+        }
+        for (Position nextPos : dict.get(nextChar)) {
+            int d = calcDist(curPos, nextPos.toArray());
+            int d2 = search(nextPos.toArray(), index + 1, S, dict);
+            ret = Math.min(ret, d + d2);
+        }
+        return ret;
     }
 
-    int[][] addNewPos(int[][] currentPos, int[] newPos) {
-        int[][] pos = Arrays.copyOf(currentPos, currentPos.length + 1);
-        pos[pos.length - 1] = newPos;
-        return pos;
+    public int solution(String S, String [] grid) {
+        int RECONST_NOT_POSSIBLE = -1;
+        if (S.length() == 0) {
+            return RECONST_NOT_POSSIBLE;
+        }
+
+        Map<String, Collection<Position>> dict = buidDict(grid);
+        System.out.println(dict);
+        if (!checkSourceString(S, dict)) {
+            return RECONST_NOT_POSSIBLE;
+        }
+
+        int totalMoves = Integer.MAX_VALUE;
+        String firstChar = S.substring(0, 1);
+        for (Position startPos : dict.get(firstChar)) {
+            totalMoves = Math.min(totalMoves, search(startPos.toArray(), 0, S, dict));
+        }
+
+        return totalMoves;
     }
 
     public static void main(String[] args) {
         Solution sol = new Solution();
+        // -1
+        System.out.println(String.format("total: %d",
+                sol.solution("", new String[] { ".A.C", ".B..", "....", "...A" })));
+        // 2
+        System.out.println(String.format("total: %d",
+                sol.solution("AC", new String[] { ".A.C", ".B..", "....", "...A" })));
+        // 6
         System.out.println(String.format("total: %d",
                 sol.solution("ABCA", new String[] { ".A.C", ".B..", "....", "...A" })));
+        // 13
         System.out.println(String.format("total: %d",
-                sol.solution("KLLRMS", new String[] { "K....", "S...L", "....R", "LX...", "XM..S" })));
+                sol.solution("KLLRML", new String[] { "K....", "S...L", "....R", "LX...", "XM..S" })));
     }
 }
